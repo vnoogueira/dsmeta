b732ed909f815db1f0e6afee7713e023d423ddd0
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-07-15 14:07:41 -0300

    Date Select

--- backend/dsmeta/src/main/java/com/vitorferreira/dsmeta/repositories/SaleRepository.java
+++ backend/dsmeta/src/main/java/com/vitorferreira/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,18 @@
 package com.vitorferreira.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vitorferreira.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	
+	@Query("SELECT obj from Sale obj where obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pegeable);
 
 }
